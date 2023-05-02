250abb683edd4d9ccbcd3d6d919fe7bd2c3b06cb
goun <gouon123@naver.com>
2023-05-02 13:34:44 +0900

    구나연

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,11 +2,40 @@ package main;
 
 import java.util.Scanner;
 
-public class MainClass {
+import event.EventSevice;
+import event.EventSeviceImpl;
+import member.MemberService;
+import member.MemberServiceImpl;
 
-	public static void main(String[] args) {
-		
-		
-	}
+
+
+public class MainClass {
+public static void main(String[] args) {
+	EventSevice ev = new EventSeviceImpl();
+	MemberService mem = new MemberServiceImpl();
+   Scanner sc = new Scanner(System.in);
+   int num ;
+   
+   while (true) {
+      System.out.println("1.회원 이동");
+      System.out.println("2. 이밴트 이동");
+      System.out.print("--->");
+      num = sc.nextInt();
+      
+      switch (num) {
+      case 1:
+    	  
+    	  mem.inputName();
+    	  mem.print();
+         break;
+      case 2:
+    	  ev.monthEventView();
+    	  ev.allEventView();
+         break;
+      }
+   }
+   
+
+}
 
 }
