baed4ab6f1f287a2859360fb686b8ebec30d66ef
swalri <keb1128ke@gmail.com>
2023-05-02 13:09:39 +0900

    event파일

---
+++ src/event/EventSeviceImpl.java
@@ -0,0 +1,24 @@
+package event;
+
+public class EventSeviceImpl implements EventSevice {
+	
+	@Override
+	public void monthEventView() {
+		// TODO Auto-generated method stub
+		System.out.println("이달의 이벤트");
+	}
+
+	@Override
+	public void allEventView() {
+		// TODO Auto-generated method stub
+		System.out.println("전체 이벤트");
+	}
+	
+	
+	
+//	public void display() {
+//		
+//	}
+	
+	
+}
